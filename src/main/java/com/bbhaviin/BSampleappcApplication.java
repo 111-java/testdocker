d547ee51f714c07fe551245e78df34000840bd82
Bhavin <bmchhda1@gmail.com>
2026-02-10 13:18:56 +0530

    code pushed

---
+++ src/main/java/com/bbhaviin/BSampleappcApplication.java
@@ -0,0 +1,13 @@
+package com.bbhaviin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSampleappcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BSampleappcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbhaviin/BSampleappcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbhaviin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSampleappcApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
